58ed322ce5097a27b67b8e8cb61d81faf90e8a0c
goderisd <goderisd@msu.edu>
2024-12-03 14:02:17 -0500

    Update flightApp_with_storytelling.py

--- flightApp_with_storytelling.py
+++ flightApp_with_storytelling.py
@@ -604,7 +604,7 @@ def delta_page():
     ax[1].set_title("PACF")
     st.pyplot(fig)
 
-""    # Fit AR(p) and Forecast
+    # Fit AR(p) and Forecast
     forecast = fit_ar_model(train_data.values, lag_range, forecast_steps)
 
     # Evaluation and Plot
@@ -778,8 +778,8 @@ def american_page():
         """)
 
     # Sidebar inputs for lag and forecast steps
-    lag_range = st.sidebar.slider("Select AR Lag Range for ACF/PACF", min_value=1, max_value=10, value=4)
-    forecast_steps = st.sidebar.slider("Forecast Steps", min_value=1, max_value=10, value=4)
+    lag_range = st.sidebar.slider("Select AR Lag Range for ACF/PACF", min_value=1, max_value=10, value=1
+    forecast_steps = st.sidebar.slider("Forecast Steps", min_value=1, max_value=10, value=1)
 
     # ACF and PACF Analysis
     train_data = filtered_non_cancelled['ARR_DELAY'][:int(len(filtered_non_cancelled) * 0.8)]  # 80% train split
@@ -1011,10 +1011,10 @@ def united_page():
     st.header("AR(p) Model Forecast on Aggregated Mean Arrival Delays")
 
     # Allow the user to choose the lag (p_value) dynamically
-    p_value = st.slider("Select the lag (p) for the AR model:", min_value=1, max_value=30, value=20)
+    p_value = st.slider("Select the lag (p) for the AR model:", min_value=1, max_value=10, value=1)
 
     # Allow user to adjust the number of forecast steps
-    forecast_steps = st.slider("Select the number of forecast steps:", min_value=1, max_value=10, value=5)
+    forecast_steps = st.slider("Select the number of forecast steps:", min_value=1, max_value=10, value=1)
 
     forecast, predictions, train_data, test_data = forecast_ar_model_with_mean(filtered_non_cancelled, p_value=p_value,
                                                                                forecast_steps=forecast_steps)
